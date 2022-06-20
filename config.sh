a80a3e06dd23a733ea127286044245f4ae9fd182
shunzi <1224522500@qq.com>
2022-06-20 11:18:52 +0800

    Add comment

--- config.sh
+++ config.sh
@@ -1,8 +1,12 @@
 # hugepage
+# 大页面 16K
 echo 16384 > /proc/sys/vm/nr_hugepages
 
 
 # cgroup
+# cgroup 限制资源使用，创建不同的资源组
+
+# kv4 限制 4G 内存，swappiness 表示最大限度使用物理内存
 mkdir /sys/fs/cgroup/memory/kv4
 echo 4G > /sys/fs/cgroup/memory/kv4/memory.limit_in_bytes
 echo 0 > /sys/fs/cgroup/memory/kv4/memory.swappiness
@@ -19,17 +23,22 @@ mkdir /sys/fs/cgroup/memory/kv80
 echo 77G > /sys/fs/cgroup/memory/kv80/memory.limit_in_bytes
 echo 0 > /sys/fs/cgroup/memory/kv80/memory.swappiness
 
+# 更改文件所属用户
 chown -R hanson:hanson /sys/fs/cgroup/memory/kv*
 
 
-# mount 
+# mount 挂载目录
+# 写日志存储的位置 /mnt/ssd
 mount /dev/sdb /mnt/ssd # where the store write log
+# 数据存储的位置 /mnt/nvm
 mount /dev/nvme0n1 /mnt/nvm # where the store keep all the records
 
 # turn off the journaling
+# 关闭两个盘上的文件系统的日志
 tune2fs -O ^has_journal /dev/nvme0n1
 tune2fs -O ^has_journal /dev/sdb
 
+# 设置 CPU 到性能模式
 # set CPU in performance mode
 cmd='-g performance'
 MAX_CPU=$((`nproc --all` - 1))
